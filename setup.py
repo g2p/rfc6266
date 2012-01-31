387fac65cf847edfafaa2c7727c77ae6aeb626e0
Gabriel <g2p.code@gmail.com>
2012-01-31 21:13:54 +0100

    Give some metadata.

--- setup.py
+++ setup.py
@@ -2,10 +2,24 @@
 from setuptools import setup
 
 setup(
+    author='Gabriel de Perthuis',
+    author_email='g2p.code+rfc6266@gmail.com',
+    description='Parse and generate Content-Disposition headers',
+    url='https://github.com/g2p/rfc6266',
+    keywords='rfc6266 Content-Disposition http attachments',
     name='rfc6266',
     version='0.0.1',  # symver
+    platforms='OS-independent',
     py_modules=['rfc6266', 'test_rfc6266'],
     install_requires=['LEPL'],
     use_2to3=True,
+    classifiers=(
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Intended Audience :: Developers',
+        'Topic :: Internet :: WWW/HTTP',
+    ),
 )
 
